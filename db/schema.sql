bcc02703ee9dddd94d9499aff9fcf18ec49ab37d
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-24 09:33:00 -0400

    database

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+USE DATABASE burgers_db;
+
+CREATE TABLE burgers 
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name varchar(150) NOT NULL,
+	devoured BOOLEAN DEFAULT FALSE NOT NULL,
+	date TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
+
